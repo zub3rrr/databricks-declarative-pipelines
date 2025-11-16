efabd49a629cb3b554c57ec13bababb42038fa5d
zub3rrr <xuberswork@gmail.com>
2025-11-16 19:48:28 +0000

    Changed the approacg for SCD Type 2 Implementation

--- Sales DWH/source_code/gold/products.py
+++ Sales DWH/source_code/gold/products.py
@@ -1,44 +1,40 @@
-import dlt
-from pyspark.sql.functions import *
+# import dlt
+# from pyspark.sql.functions import *
 
-@dlt.table(
-    comment="SCD2 merge table for product price changes.",
-    name="products_history")
-def products_history():
-    src = spark.read.table("transformed_product").withColumn(
-        "start_date", current_timestamp()
-    ).withColumn(
-        "end_date", lit(None).cast("timestamp")
-    )
 
-    target = "product_history"
-    spark.sql(f"""
-        MERGE INTO {target} AS tgt
-        USING (SELECT * FROM transformed_product) as src
-        ON tgt.product_id = src.product_id 
-        WHEN MATCHED AND tgt.price <> src.price THEN
-          UPDATE SET
-            tgt.start_date = src.last_updated,
-            tgt.end_date = null
-        WHEN NOT MATCHED THEN
-          INSERT (
-            product_id,
-            product_name,
-            product_category,
-            price,
-            last_updated,
-            start_date,
-            end_date
-          )
-          VALUES (
-            src.product_id,
-            src.product_name,
-            src.product_category,
-            src.price,
-            src.last_updated,
-            src.last_updated,
-            NULL
-          )
-    """)
+# @dlt.table(
+#     comment="Product SCD2 History Table",
+#     name = "products_history"
+# )
+# def products_history():
+#     return (
+#         spark.createDataFrame([], """
+#             product_id STRING,
+#             product_name STRING,
+#             product_category STRING,
+#             price DECIMAL(10,2),
+#             last_updated TIMESTAMP,
+#             start_date TIMESTAMP,
+#             end_date TIMESTAMP,
+#             current_flag INT
+#         """)
+#     )
 
-    return spark.read.table(target)
+
+# def scd_type():
+#     status = spark.sql("""
+#         MERGE INTO products_history AS tgt
+#         USING (SELECT * FROM transformed_product) AS src
+#         ON tgt.product_id = src.product_id
+#         WHEN MATCHED AND tgt.price <> src.price THEN
+#           UPDATE SET
+#             tgt.start_date = src.last_updated,
+#             tgt.end_date = NULL
+#         WHEN NOT MATCHED THEN
+#           INSERT (product_id, product_name, product_category, price, last_updated, start_date, end_date)
+#           VALUES (src.product_id, src.product_name, src.product_category, src.price, src.last_updated, src.last_updated, NULL)
+#     """)
+#     return status
+  
+
+# scd_type()
