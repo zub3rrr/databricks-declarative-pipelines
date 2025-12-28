ad541aa3e62a37eb1e5f4bec4dda5f5753de7321
zub3rrr <xuberswork@gmail.com>
2025-12-28 13:22:19 +0000

    Corrected and Validated | gold/products.py

--- Sales DWH/source_code/gold/products.py
+++ Sales DWH/source_code/gold/products.py
@@ -1,40 +1,14 @@
-# import dlt
-# from pyspark.sql.functions import *
+import dlt
+from pyspark.sql.functions import *
+
+dlt.create_streaming_table(
+    name="products_za")
 
 
-# @dlt.table(
-#     comment="Product SCD2 History Table",
-#     name = "products_history"
-# )
-# def products_history():
-#     return (
-#         spark.createDataFrame([], """
-#             product_id STRING,
-#             product_name STRING,
-#             product_category STRING,
-#             price DECIMAL(10,2),
-#             last_updated TIMESTAMP,
-#             start_date TIMESTAMP,
-#             end_date TIMESTAMP,
-#             current_flag INT
-#         """)
-#     )
-
-
-# def scd_type():
-#     status = spark.sql("""
-#         MERGE INTO products_history AS tgt
-#         USING (SELECT * FROM transformed_product) AS src
-#         ON tgt.product_id = src.product_id
-#         WHEN MATCHED AND tgt.price <> src.price THEN
-#           UPDATE SET
-#             tgt.start_date = src.last_updated,
-#             tgt.end_date = NULL
-#         WHEN NOT MATCHED THEN
-#           INSERT (product_id, product_name, product_category, price, last_updated, start_date, end_date)
-#           VALUES (src.product_id, src.product_name, src.product_category, src.price, src.last_updated, src.last_updated, NULL)
-#     """)
-#     return status
-  
-
-# scd_type()
+dlt.create_auto_cdc_flow(
+    target="products_za",
+    source="transformed_product",
+    keys=["product_id"],
+    sequence_by="last_updated",
+    stored_as_scd_type=2
+)
